6464d16683a3050898bc1e31d9f0f3f7745537ad
Stanislav3791 <79421048+Stanislav3791@users.noreply.github.com>
2021-04-18 19:00:55 +0200

    add arrays

--- my-message-generator/script.js
+++ my-message-generator/script.js
@@ -1,1 +1,6 @@
-pervaia strrrrr ffhjgjh
+const pronoun = ['I','You', 'He', 'She', 'We', 'They'];
+const mod = ['can', 'must', 'need'];
+const verb = ['go', 'swim', 'ride', 'fly', 'eat', 'stand', 'poop', 'cry', 'laugh', 'run'];
+let to = 'to';
+const union = ['in', 'on', 'under', 'over', 'inside', 'outside', 'from', 'behind', 'before'];
+const where = [];
