e6370e2fc9041c45ba5e7ecbebec0533422e1197
Stanislav3791 <79421048+Stanislav3791@users.noreply.github.com>
2021-04-18 20:01:35 +0200

    final?

--- my-message-generator/script.js
+++ my-message-generator/script.js
@@ -1,6 +1,15 @@
 const pronoun = ['I','You', 'He', 'She', 'We', 'They'];
 const mod = ['can', 'must', 'need'];
 const verb = ['go', 'swim', 'ride', 'fly', 'eat', 'stand', 'poop', 'cry', 'laugh', 'run'];
-let to = 'to';
-const union = ['in', 'on', 'under', 'over', 'inside', 'outside', 'from', 'behind', 'before'];
-const where = [];
+const union = ['in', 'on', 'under', 'over', 'inside', 'outside', 'from', 'behind', 'before', 'to', 'till'];
+const where = ['water', 'food', 'pit', 'lake', 'sea', 'mud', 'city', 'house', 'work', 'road', 'table', 'window', 'end', 'beginning'];
+
+function main () {
+let pikPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+let pikMode = mod[Math.floor(Math.random() * mod.length)];
+let pikVerb = verb[Math.floor(Math.random() * verb.length)];
+let pikUnion = union[Math.floor(Math.random() * union.length)];
+let pikWhere = where[Math.floor(Math.random() * where.length)];
+console.log(`${pikPronoun} ${pikMode} ${pikVerb} ${pikUnion} ${pikWhere}.`);
+};
+main();
